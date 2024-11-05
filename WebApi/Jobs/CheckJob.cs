cf2732b3e54c099c85d648faded3d064006f65fd
gonzobard777 <gonzo.bard@dwfe.ru>
2024-11-05 15:37:48 +0300

    .

---
+++ WebApi/Jobs/CheckJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace WebApi.Jobs;
+
+public class CheckJob : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine(@$"Job executed: {DateTime.Now}");
+        
+        return Task.CompletedTask;
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Quartz;
+using WebApi.Jobs;
+
 namespace WebApi;
 
 public class Program
@@ -6,16 +9,30 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+
         // Add services to the container.
 
         builder.Services.AddControllers();
 
+        builder.Services.AddQuartz(q =>
+        {
+            // Just use the name of your job that you created in the Jobs folder.
+            var jobKey = new JobKey("CheckJob");
+            q.AddJob<CheckJob>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("CheckJob-trigger")
+                //This Cron interval can be described as "run every minute" (when second is zero)
+                .WithCronSchedule("0 34 * ? * *")
+            );
+        });
+        builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
         var app = builder.Build();
 
+
         // Configure the HTTP request pipeline.
-
-        app.UseAuthorization();
-
 
         app.MapControllers();
 
